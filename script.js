a01529e9366eb114b21a237128648a7495c05eed
ShivamVerma22 <vermashivam527@gmail.com>
2020-09-08 16:14:23 +0530

    Responsive done

--- script.js
+++ script.js
@@ -9,14 +9,3 @@ document.querySelector('#close').addEventListener('click', function() {
     document.querySelector('.right').classList.toggle('active');
     document.querySelector('#close').classList.toggle('active');
 })
-
-if(/Android [4-6]/.test(navigator.appVersion)) {
-    window.addEventListener("resize", function() {
-       if(document.activeElement.tagName=="INPUT" || document.activeElement.tagName=="TEXTAREA") {
-          window.setTimeout(function() {
-             document.activeElement.scrollIntoViewIfNeeded();
-          },0);
-       }
-    })
-}
-  
